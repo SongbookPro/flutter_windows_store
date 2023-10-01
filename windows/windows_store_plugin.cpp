8438f8fa741d4d2ff9fac82c3514630570423758
Jonathan Salmon <jonathan.salmon@hotmail.co.uk>
2023-10-01 23:46:56 +0100

    Basic windows store function

--- windows/windows_store_plugin.cpp
+++ windows/windows_store_plugin.cpp
@@ -13,6 +13,16 @@
 #include <memory>
 #include <sstream>
 
+#include <iostream>
+#include <ppltasks.h>
+#include <winrt/Windows.Services.Store.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+using namespace winrt;
+using namespace Windows::Services::Store;
+using namespace Windows::Foundation;
+using namespace Concurrency;
+
 namespace windows_store {
 
 // static
@@ -41,6 +51,39 @@ void WindowsStorePlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
   if (method_call.method_name().compare("getPlatformVersion") == 0) {
+
+          printf("Running co-routine\n");
+    concurrency::create_task([]
+    {
+              try
+        {
+    StoreContext storeContext = StoreContext::GetDefault();
+    auto licenseAsync = storeContext.GetAppLicenseAsync();
+    auto license = licenseAsync.get();
+    // concurrency::create_task(storeContext.GetAppLicenseAsync()).then([](StoreAppLicense license)
+    // {
+    bool isActive = license.IsActive();
+    bool isTrial = license.IsTrial();
+    auto skuStoreId = license.SkuStoreId();
+    auto trialTimeRemaining = license.TrialTimeRemaining();
+
+        printf("Is active = %d\n", isActive);
+        printf("Is trial = %d\n", isTrial);
+        
+    // }, task_continuation_context::get_current_winrt_context());
+
+        }
+            catch (winrt::hresult_error const& ex)
+        {
+          printf("Error caught");
+            winrt::hresult hr = ex.code(); // HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND).
+            winrt::hstring message = ex.message(); // The system cannot find the file specified.
+        }
+
+
+    }
+    );
+
     std::ostringstream version_stream;
     version_stream << "Windows ";
     if (IsWindows10OrGreater()) {
